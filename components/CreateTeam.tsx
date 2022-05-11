5117cb2d118e66831d8a58e45059bf134f3d52dd
Thomas WEst <thom8750@stud.kea.dk>
2022-05-11 14:10:31 +0200

    members shown on team

--- components/CreateTeam.tsx
+++ components/CreateTeam.tsx
@@ -1,8 +1,8 @@
-import React, {useRef, useState} from 'react'
-import {useUser} from './../lib/firebase/useUser'
-import {db} from './../lib/firebase/initFirebase'
-import {doc, setDoc, collection, documentId} from 'firebase/firestore'
-import {getAuth} from '@firebase/auth'
+import React, { useRef, useState } from 'react'
+import { useUser } from './../lib/firebase/useUser'
+import { db } from './../lib/firebase/initFirebase'
+import { doc, setDoc, collection, documentId, getDoc } from 'firebase/firestore'
+import { getAuth } from '@firebase/auth'
 import {
   Modal,
   ModalOverlay,
@@ -18,10 +18,10 @@ import {
   Input,
   useToast,
 } from '@chakra-ui/react'
-import {useAuth} from '../lib/authContext'
-import {useForm} from "react-hook-form";
-import {getDownloadURL, ref, uploadBytes} from "firebase/storage";
-import {storage} from "../lib/firebase/initFirebase";
+import { useAuth } from '../lib/authContext'
+import { useForm } from "react-hook-form";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+import { storage } from "../lib/firebase/initFirebase";
 import AsyncSelect from 'react-select/async';
 import Select from 'react-select';
 
@@ -34,16 +34,20 @@ const CreateTeam = () => {
   const [lowRank, setLowRank] = useState<string>("")
   const [highRank, setHighRank] = useState<string>("")
 
-  const {isOpen, onOpen, onClose} = useDisclosure()
+  const { isOpen, onOpen, onClose } = useDisclosure()
   const toast = useToast()
   const initialRef = useRef()
-  const {register, handleSubmit} = useForm();
+  const { register, handleSubmit } = useForm();
 
   const createTeam = async (url) => {
 
     try {
       const myCollRef = collection(db, "teams");
       const myDocRef = doc(myCollRef);
+
+      const myUserRef = doc(db, 'users', auth.currentUser.uid)
+
+
 
       await setDoc(myDocRef, {
         teamName: teamName,
@@ -54,6 +58,11 @@ const CreateTeam = () => {
         owner: auth.currentUser.uid,
         id: myDocRef.id
       });
+
+      await setDoc(myUserRef, {
+        team: myDocRef.id
+      }, { merge: true });
+     
 
       toast({
         title: "Success!",
@@ -92,16 +101,16 @@ const CreateTeam = () => {
 
 
   const ranks = [
-    {value: "faceit1.png", image: "https://firebasestorage.googleapis.com/v0/b/ehubladder.appspot.com/o/images%2Franks%2Ffaceit1.png?alt=media&token=0ab5dd1a-5833-46d7-8b2e-29d5fba193d3"},
-    {value: "faceit2.png", image: "https://firebasestorage.googleapis.com/v0/b/ehubladder.appspot.com/o/images%2Franks%2Ffaceit2.png?alt=media&token=00b9e38e-4467-42f4-a880-41854c1fceae"},
-    {value: "faceit3.png", image: "https://firebasestorage.googleapis.com/v0/b/ehubladder.appspot.com/o/images%2Franks%2Ffaceit3.png?alt=media&token=a20b8bab-e961-46e2-ac57-a55a2b5a5e0b"},
-    {value: "faceit4.png", image: "https://firebasestorage.googleapis.com/v0/b/ehubladder.appspot.com/o/images%2Franks%2Ffaceit4.png?alt=media&token=e4b220b9-f881-426d-8be4-942872b2a9d6"},
-    {value: "faceit5.png", image: "https://firebasestorage.googleapis.com/v0/b/ehubladder.appspot.com/o/images%2Franks%2Ffaceit5.png?alt=media&token=40cedbd1-2bb9-4ad3-9e4e-0b05ede1abf5"},
-    {value: "faceit6.png", image: "https://firebasestorage.googleapis.com/v0/b/ehubladder.appspot.com/o/images%2Franks%2Ffaceit6.png?alt=media&token=7213062c-2db9-47a6-84af-ca0aa20d0782"},
-    {value: "faceit7.png", image: "https://firebasestorage.googleapis.com/v0/b/ehubladder.appspot.com/o/images%2Franks%2Ffaceit7.png?alt=media&token=c8dfd06d-d083-4798-984e-e28f1a0b5335"},
-    {value: "faceit8.png", image: "https://firebasestorage.googleapis.com/v0/b/ehubladder.appspot.com/o/images%2Franks%2Ffaceit8.png?alt=media&token=1fd2dce3-7261-46c2-8a6d-2ee0963f9595"},
-    {value: "faceit9.png", image: "https://firebasestorage.googleapis.com/v0/b/ehubladder.appspot.com/o/images%2Franks%2Ffaceit9.png?alt=media&token=952626a6-4e7d-4bc5-87b2-4d67985a4b3c"},
-    {value: "faceit10.png", image: "https://firebasestorage.googleapis.com/v0/b/ehubladder.appspot.com/o/images%2Franks%2Ffaceit10.png?alt=media&token=27ca1c97-0758-489e-8e2d-023bef010874"},
+    { value: "faceit1.png", image: "https://firebasestorage.googleapis.com/v0/b/ehubladder.appspot.com/o/images%2Franks%2Ffaceit1.png?alt=media&token=0ab5dd1a-5833-46d7-8b2e-29d5fba193d3" },
+    { value: "faceit2.png", image: "https://firebasestorage.googleapis.com/v0/b/ehubladder.appspot.com/o/images%2Franks%2Ffaceit2.png?alt=media&token=00b9e38e-4467-42f4-a880-41854c1fceae" },
+    { value: "faceit3.png", image: "https://firebasestorage.googleapis.com/v0/b/ehubladder.appspot.com/o/images%2Franks%2Ffaceit3.png?alt=media&token=a20b8bab-e961-46e2-ac57-a55a2b5a5e0b" },
+    { value: "faceit4.png", image: "https://firebasestorage.googleapis.com/v0/b/ehubladder.appspot.com/o/images%2Franks%2Ffaceit4.png?alt=media&token=e4b220b9-f881-426d-8be4-942872b2a9d6" },
+    { value: "faceit5.png", image: "https://firebasestorage.googleapis.com/v0/b/ehubladder.appspot.com/o/images%2Franks%2Ffaceit5.png?alt=media&token=40cedbd1-2bb9-4ad3-9e4e-0b05ede1abf5" },
+    { value: "faceit6.png", image: "https://firebasestorage.googleapis.com/v0/b/ehubladder.appspot.com/o/images%2Franks%2Ffaceit6.png?alt=media&token=7213062c-2db9-47a6-84af-ca0aa20d0782" },
+    { value: "faceit7.png", image: "https://firebasestorage.googleapis.com/v0/b/ehubladder.appspot.com/o/images%2Franks%2Ffaceit7.png?alt=media&token=c8dfd06d-d083-4798-984e-e28f1a0b5335" },
+    { value: "faceit8.png", image: "https://firebasestorage.googleapis.com/v0/b/ehubladder.appspot.com/o/images%2Franks%2Ffaceit8.png?alt=media&token=1fd2dce3-7261-46c2-8a6d-2ee0963f9595" },
+    { value: "faceit9.png", image: "https://firebasestorage.googleapis.com/v0/b/ehubladder.appspot.com/o/images%2Franks%2Ffaceit9.png?alt=media&token=952626a6-4e7d-4bc5-87b2-4d67985a4b3c" },
+    { value: "faceit10.png", image: "https://firebasestorage.googleapis.com/v0/b/ehubladder.appspot.com/o/images%2Franks%2Ffaceit10.png?alt=media&token=27ca1c97-0758-489e-8e2d-023bef010874" },
 
   ];
 
@@ -122,7 +131,7 @@ const CreateTeam = () => {
         backgroundColor="gray.900"
         color="white"
         fontWeight="medium"
-        _hover={{bg: 'gray.700'}}
+        _hover={{ bg: 'gray.700' }}
         _active={{
           bg: 'gray.800',
           transform: 'scale(0.95)'
