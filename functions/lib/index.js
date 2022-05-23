1ed2b1fe433c1899a0b989118c32cc1f22556ed2
abdulahi roble <abdulahi_2610@hotmail.com>
2022-05-23 22:05:31 +0200

    Change addParticipant function

--- functions/lib/index.js
+++ functions/lib/index.js
@@ -32,57 +32,32 @@ exports.getAllTournaments = functions.https.onRequest(async (req, res) => {
         res.status(500).json({ message: err });
     }
 });
-// Create a POST cloud function that takes in a parameter with the name of the participant and adds them to the tournament. The function should return the participant object.
-// export const addParticipant = functions.https.onRequest(async (req, res) => {
-//   res.set('Access-Control-Allow-Origin', '*');
-//   try {
-//     process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
-//     const response = await axios(
-//       `https://api.challonge.com/v1/tournaments/11154801/participants.json`,
-//       {
-//         params: { api_key: process.env.CHALLONGE_API_KEY },
-//         method: 'POST',
-//         headers: {
-//           Accept: 'application/json',
-//           'Content-Type': 'application/json;charset=UTF-8',
-//         },
-//         data: {
-//           participant: {
-//             name: req.body.name,
-//           },
-//         },
-//       }
-//     );
-//     res.status(200).json(response.data);
-//   } catch (err) {
-//     res.status(500).json({ message: err });
-//   }
-// });
 exports.addParticipant = functions.https.onRequest(async (req, res) => {
     res.set('Access-Control-Allow-Origin', '*');
     process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
     try {
-        cors()(req, res, async () => {
-            process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
-            res.set('Access-Control-Allow-Origin', '*');
-            const response = await (0, axios_1.default)(`https://api.challonge.com/v1/tournaments/11154801/participants.json`, {
-                params: { api_key: process.env.CHALLONGE_API_KEY },
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json', "Access-Control-Allow-Origin": "*" },
-                data: {
-                    participant: {
-                        name: req.body.name,
-                    },
+        process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+        res.set('Access-Control-Allow-Origin', '*');
+        const response = await (0, axios_1.default)(`https://api.challonge.com/v1/tournaments/11154801/participants.json`, {
+            params: { api_key: process.env.CHALLONGE_API_KEY },
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json', "Access-Control-Allow-Origin": "*" },
+            data: {
+                participant: {
+                    name: req.body.name,
                 },
-            });
-            res.status(200).json(response.data);
+            },
         });
-        // const response = await axios(
+        res.status(200).json(response.data);
+        // cors()(req, res, async () => {
+        //   process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+        //   res.set('Access-Control-Allow-Origin', '*');
+        //       const response = await axios(
         //   `https://api.challonge.com/v1/tournaments/11154801/participants.json`,
         //   {
         //     params: { api_key: process.env.CHALLONGE_API_KEY },
         //     method: 'POST',
-        //     headers: {'Content-Type': 'application/json'},
+        //     headers: {'Content-Type': 'application/json', "Access-Control-Allow-Origin": "*"},
         //     data: {
         //       participant: {
         //         name: req.body.name,
@@ -91,14 +66,10 @@ exports.addParticipant = functions.https.onRequest(async (req, res) => {
         //   }
         // );
         // res.status(200).json(response.data);
+        // });
     }
     catch (err) {
         res.status(500).json({ message: err });
     }
 });
-// exports.addMessage = functions.https.onRequest((req, res) => {
-//   cors()(req, res, () => {
-//     return res.json({status: 'ok'});
-//   });
-// });
 //# sourceMappingURL=index.js.map
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -38,41 +38,13 @@ export const getAllTournaments = functions.https.onRequest(async (req, res) => {
   }
 });
 
-// Create a POST cloud function that takes in a parameter with the name of the participant and adds them to the tournament. The function should return the participant object.
-
-// export const addParticipant = functions.https.onRequest(async (req, res) => {
-//   res.set('Access-Control-Allow-Origin', '*');
-
-//   try {
-//     process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
-//     const response = await axios(
-//       `https://api.challonge.com/v1/tournaments/11154801/participants.json`,
-//       {
-//         params: { api_key: process.env.CHALLONGE_API_KEY },
-//         method: 'POST',
-//         headers: {
-//           Accept: 'application/json',
-//           'Content-Type': 'application/json;charset=UTF-8',
-//         },
-//         data: {
-//           participant: {
-//             name: req.body.name,
-//           },
-//         },
-//       }
-//     );
-//     res.status(200).json(response.data);
-//   } catch (err) {
-//     res.status(500).json({ message: err });
-//   }
-// });
 
 export const addParticipant = functions.https.onRequest(async (req, res) => {
   res.set('Access-Control-Allow-Origin', '*');
   process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
+
   try {
-    cors()(req, res, async () => {
       process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
       res.set('Access-Control-Allow-Origin', '*');
       
@@ -89,16 +61,20 @@ export const addParticipant = functions.https.onRequest(async (req, res) => {
         },
       }
     );
+    
     res.status(200).json(response.data);
 
-    });
 
-    // const response = await axios(
+    // cors()(req, res, async () => {
+    //   process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+    //   res.set('Access-Control-Allow-Origin', '*');
+      
+    //       const response = await axios(
     //   `https://api.challonge.com/v1/tournaments/11154801/participants.json`,
     //   {
     //     params: { api_key: process.env.CHALLONGE_API_KEY },
     //     method: 'POST',
-    //     headers: {'Content-Type': 'application/json'},
+    //     headers: {'Content-Type': 'application/json', "Access-Control-Allow-Origin": "*"},
     //     data: {
     //       participant: {
     //         name: req.body.name,
@@ -106,14 +82,12 @@ export const addParticipant = functions.https.onRequest(async (req, res) => {
     //     },
     //   }
     // );
+    
     // res.status(200).json(response.data);
+
+    // });
+
   } catch (err) {
     res.status(500).json({ message: err });
   }
 });
-
-// exports.addMessage = functions.https.onRequest((req, res) => {
-//   cors()(req, res, () => {
-//     return res.json({status: 'ok'});
-//   });
-// });
